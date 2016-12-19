cc4e6fd1c90aaed7bad3e03efa21c6418a9d34ce
Irfan Rasool <irfan@web-flight.nl>
2016-12-19 11:39:41 +0530

    #comment add reduce method withe examples

--- reduce.js
+++ reduce.js
@@ -10,10 +10,30 @@
 // console.log(balancedParans(')('));
 
 //Use the 'reduce' helper to find the sum of all the distances traveled.  Assign the result to the variable 'totalDistance'
-let trips = [{ distance: 34 }, { distance: 12 } , { distance: 1 }];
+// let trips = [{ distance: 34 }, { distance: 12 } , { distance: 1 }];
 
-let totalDistance = trips.reduce((prev, trip) => {
-       return prev + trip.distance;
-}, 0);
+// let totalDistance = trips.reduce((prev, trip) => {
+//        return prev + trip.distance;
+// }, 0);
 
-console.log(totalDistance);
+// console.log(totalDistance);
+ 
+ // Use the 'reduce' helper to create an object that tallies the number of sitting and standing desks.  
+ // The object returned should have the form '{ sitting: 3, standing: 2 }'.  The initial value has been provided to you.
+ // Hint: Don't forget to return the accumulator object (the first argument to the iterator function)
+ // 
+ // 
+ var desks = [
+  { type: 'sitting' },
+  { type: 'standing' },
+  { type: 'sitting' },
+  { type: 'sitting' },
+  { type: 'standing' }
+];
+
+var deskTypes = desks.reduce(function(prevObj, desk) {
+    if (desk.type === 'sitting') { ++prevObj.sitting;}
+    if (desk.type === 'standing') { ++prevObj.standing;}
+    return prevObj;
+}, { sitting: 0, standing: 0 });
+ console.log(deskTypes);
